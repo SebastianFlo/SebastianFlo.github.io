b3b278c51a9aff7431f252c89e3309df084818c5
Sebastian Florian <rebardesigndk@gmail.com>
2018-11-10 18:43:21 +0100

    feature(State): Add component redux

---
+++ client/src/modules/core/HelloWorld/HelloWorld.state.js
@@ -0,0 +1,19 @@
+// components/App.js
+import { connect } from 'redux-vue';
+
+import HelloWorld from './HelloWorld.vue';
+
+const mapStateToProps = (state) => ({
+    todos: state.todos
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    addTodo(todo) {
+        dispatch({
+            type: 'ADD_TODO',
+            data: { todo }
+        })
+    }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(HelloWorld)
--- client/src/modules/home/Home.vue
+++ client/src/modules/home/Home.vue
@@ -1,17 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../../assets/logo.png">
-    <HelloWorld msg="Sebastian Florian - Web Development"/>
-  </div>
+    <div class="home">
+        <img alt="Vue logo"
+            src="../../assets/logo.png">
+        <HelloWorld msg="Sebastian Florian - Web Development" />
+    </div>
 </template>
 
 <script>
-import HelloWorld from '@/modules/core/HelloWorld/HelloWorld.vue'
+    import HelloWorld from '@/modules/core/HelloWorld/HelloWorld.state.js';
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'home',
+        components: {
+            HelloWorld
+        }
+    }
 </script>
